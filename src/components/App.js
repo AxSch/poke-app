08683ef11691cc5b511d37e87b9173f65c2de08f
Alexander Schuneman <alex_schuneman@outlook.com>
2017-11-13 14:43:49 +0000

    Initial

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Aiiiight g this is liiiiive!
+      </div>
+    );
+  }
+}
+
+export default App;
