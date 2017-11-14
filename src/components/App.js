7b8684d000f262b0b219e735f8f6d1b85aebbd15
Alexander Schuneman <alex_schuneman@outlook.com>
2017-11-14 17:09:07 +0000

    feat: Implemented Stateless component - DetailView
    Handles the detail view in the app, along with
    styling. Detail view shows the pokemon info
    when cell is clicked.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import PokeList from './PokeList';
+import DetailView from './DetailView';
 
 class App extends Component {
     constructor() {
@@ -11,6 +12,7 @@ class App extends Component {
         return (
             <div className="App">
                 <PokeList />
+                <DetailView />
             </div>
         );
     }
